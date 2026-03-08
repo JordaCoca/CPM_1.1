14bf57055cceff68400ce5d7a3eb39f8262dcfa6
Jordà Coca <jordacrodriguez02@gmail.com>
2026-03-08 19:48:35 +0100

    Retoque

---
+++ scripts_ejecucion/seq_orca_solo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CODIGO_DIR="secuencial/codigo"
+RESULT_DIR="secuencial/resultados"
+
+mkdir -p "$RESULT_DIR"
+
+echo "Usando solo orca..."
+
+for SRC in $CODIGO_DIR/*.c; do
+
+    NAME=$(basename "$SRC" .c)
+    BIN="$CODIGO_DIR/$NAME"
+
+    echo "Compilando $SRC"
+    cc -O "$SRC" -o "$BIN" -lm -lpthread
+
+    if [ $? -ne 0 ]; then
+        echo "Error compilando $SRC"
+        continue
+    fi
+
+    echo "Ejecutando $NAME en ORCA"
+
+    {
+        echo "Programa: $NAME"
+        echo "Máquina: orca"
+        echo "----------------"
+        /usr/bin/time -f "Tiempo_total: %e segundos" \
+        srun -p orca -c 1 ./$BIN
+    } &> "$RESULT_DIR/orca_${NAME}"
+
+done
+
+echo "Ejecución secuencial completada."
