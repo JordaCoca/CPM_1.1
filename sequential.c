4ddd576647676c952c58ae6f4ba33095459b598d
Jordà Coca <jordacrodriguez02@gmail.com>
2026-03-09 20:50:38 +0100

    Etiquetadas

--- sequential.c
+++ sequential.c
@@ -29,15 +29,15 @@ int fD[N];
 	  // Miramos todos los elementos
 	  for (i=0;i<fN; i++)
 	   {
-		min = 0;						 // Indice del centroide (cluster) con el que se guarda una distancia menor (inicialmente 0)
-		dif = abs(fV[i] -fR[0]);		 // Calculamos la distancia respecto el elemento actual y el centroide (un primer centroide, inicializamos basicamente el dif_menor)
-		for (j=1;j<fK;j++)				 // Miramos los otros clusters
-		  if (abs(fV[i] -fR[j]) < dif)   // SI la fistancia entre este elemento y el siguiente centroide (cluster), es menor que el anterior dif_min:
-		   {	
-			min = j;					// El siguiente cluster pasa a ser el cluster_minimo actual del elemento actual (de momento)
-			dif = abs(fV[i] -fR[j]);	// La distancia minima es la nueva encontrada
-		   }
-		fD[i] = min;					// Asignamos finalmente el elemento actual el cluster con el que guarde menos distancia con su centroide
+		min = 0;						 // Indice del centroide (cluster) con el que se guarda una distancia menor (inicialmente 0)												|S1|
+		dif = abs(fV[i] -fR[0]);		 // Calculamos la distancia respecto el elemento actual y el centroide (un primer centroide, inicializamos basicamente el dif_menor)		|S2|
+		for (j=1;j<fK;j++)				 // Miramos los otros clusters																												|S3|
+		  if (abs(fV[i] -fR[j]) < dif)   // SI la fistancia entre este elemento y el siguiente centroide (cluster), es menor que el anterior dif_min:								|S4|
+		   {																																							//			|S5|
+			min = j;					// El siguiente cluster pasa a ser el cluster_minimo actual del elemento actual (de momento)												|S6|
+			dif = abs(fV[i] -fR[j]);	// La distancia minima es la nueva encontrada																								|S7|
+		   }	
+		fD[i] = min;					// Asignamos finalmente el elemento actual el cluster con el que guarde menos distancia con su centroide									|S8|	
 	   }
 	  
 	  
@@ -46,26 +46,26 @@ int fD[N];
 	  //-fA[]: Almacenar cuantos elementos hay en cada cluster
 	  
 	  // Inicializamos los acumuladres a 0
-	  for(i=0;i<fK;i++)
-		fS[i] = fA[i] = 0;
+	  for(i=0;i<fK;i++)				//		|S9|
+		fS[i] = fA[i] = 0;			//		|S10|
 
 	  // Fd[]: Es un array que tiene tantas posiciones como elementos a asignar, cada casilla indica que elemento (i) pertenece a que cluster (el valor como tal (un indice))
 	  for(i=0;i<fN;i++)
 	   {
-		fS[fD[i]] += fV[i];		// fF[indice_cluster_al_q_pertenece_este_elemento] = valor_numerico_de_este_elemento
-		fA[fD[i]] ++;			// fA[indice_cluster_al_q_pertenece_este_elemento]++, indicamos que tiene 1 elemento más ese cluster
+		fS[fD[i]] += fV[i];		// fF[indice_cluster_al_q_pertenece_este_elemento] = valor_numerico_de_este_elemento						|S11|
+		fA[fD[i]] ++;			// fA[indice_cluster_al_q_pertenece_este_elemento]++, indicamos que tiene 1 elemento más ese cluster		|S12|
 	   }
 	  
 	  // Recalculamos los centroides con la media de los elementos asignados
-	  dif = 0;								// mide cuanto cambiaron los centroides en esta iteración, condicion d parada
+	  dif = 0;								// mide cuanto cambiaron los centroides en esta iteración, condicion d parada					|S13|
 	  for(i=0;i<fK;i++)						// miramos todos los clusters (num d clusters = fK)
 	   {
-		t = fR[i];							// pillamos el centroide actual y lo guardamos en 't'
-		if (fA[i]) fR[i] = fS[i]/fA[i];     // si el cluster tiene algun miembro, calculamos entonces el nuevo centroide --> media d todos los valores numericos d todos los elementos que forman parte del cluster
-		dif += abs(t - fR[i]);				// acumulamos diferencia absoluta entre el centroide anterior ('t') y el nuevo
+		t = fR[i];							// pillamos el centroide actual y lo guardamos en 't'																															|S14|
+		if (fA[i]) fR[i] = fS[i]/fA[i];     // si el cluster tiene algun miembro, calculamos entonces el nuevo centroide --> media d todos los valores numericos d todos los elementos que forman parte del cluster			|S15|
+		dif += abs(t - fR[i]);				// acumulamos diferencia absoluta entre el centroide anterior ('t') y el nuevo																									|S16|
 	   }
-	   iter ++;								// num d iteraciones totales (cada iteración implica asignar todos los elementos, y recalcular los centroides)
-	 } while(dif);							// Hacemos esto siempre y cuando se mejoren los centroides, cuando se estabilicen llegamos al gin (cuando los centroides no hayan cambiado entre iteraciones, ese es el final)
+	   iter ++;								// num d iteraciones totales (cada iteración implica asignar todos los elementos, y recalcular los centroides)																	|S17|	
+	 } while(dif);							// Hacemos esto siempre y cuando se mejoren los centroides, cuando se estabilicen llegamos al gin (cuando los centroides no hayan cambiado entre iteraciones, ese es el final)  |S18|
 
 	 printf("iter %d\n",iter);
 }
@@ -93,21 +93,21 @@ void qs(int ii, int fi, long fV[], int fA[])
   // mientras que no lleguemos al final
   while (i <= f)
    {
-    if (vtmp < pi) {			// si valor temporal menor q el pivote, se coloca a la izquierda del pivote
-	  fV[i-1] = vtmp;			// colocamos el valor en la posición anterior
-	  fA[i-1] = vta;			// movemos tmb el valor asociado
-	  i ++;						// pasamos al siguiente elemento
-	  vtmp = fV[i];				// cargamos el siguiente valor
-	  vta = fA[i];
+    if (vtmp < pi) {			// si valor temporal menor q el pivote, se coloca a la izquierda del pivote			|S1|
+	  fV[i-1] = vtmp;			// colocamos el valor en la posición anterior    									|S2|
+	  fA[i-1] = vta;			// movemos tmb el valor asociado													|S3|
+	  i ++;						// pasamos al siguiente elemento													|S4|
+	  vtmp = fV[i];				// cargamos el siguiente valor														|S5|
+	  vta = fA[i];				//																					|S6|
 	}
-    else {						// Si valor es mayor o igual a pivote debe ir a su derecha
-	  vfi = fV[f];				// guardamos el elemento del extremo derecho antes d sobreescribir
-	  vfa = fA[f];
-	  fV[f] = vtmp;				// valor actual se mueve a la zona derecha
-	  fA[f] = vta;
-	  f --;						// movemos el extremo derecho, 1 a la izquierda
-	  vtmp = vfi;				// pillamos el valor que estaba al final
-	  vta = vfa;
+    else {						// Si valor es mayor o igual a pivote debe ir a su derecha							|S7|
+	  vfi = fV[f];				// guardamos el elemento del extremo derecho antes d sobreescribir					|S8|
+	  vfa = fA[f];				//																					|S9|
+	  fV[f] = vtmp;				// valor actual se mueve a la zona derecha											|S10|											
+	  fA[f] = vta;				//																					|S11|						
+	  f --;						// movemos el extremo derecho, 1 a la izquierda										|S12|
+	  vtmp = vfi;				// pillamos el valor que estaba al final											|S13|
+	  vta = vfa;				//																					|S14|
 	}
    }
   
