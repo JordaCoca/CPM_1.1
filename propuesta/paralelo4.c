b7b630461a093a0425777d1b5340d01b26da66a4
JordaC <jorda.coca@estudiants.urv.cat>
2026-06-03 12:27:17 +0200

    Retoque

--- propuesta/paralelo4.c
+++ propuesta/paralelo4.c
@@ -1,5 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <omp.h>
+
+#define N 600000
+#define G 200
+
+long V[N];
+long R[G];
+int A[G];
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
 
 #define N 600000
 #define G 200
@@ -37,7 +49,6 @@ void kmean(int fN, int fK, long fV[], long fR[], int fA[])
                     min_dif = curr_dif;
                 }
             }
-            
             fS[min] += fV[i];
             fA[min] += 1;
         }
@@ -63,75 +74,48 @@ void kmean(int fN, int fK, long fV[], long fR[], int fA[])
     printf("iter %d\n", iter);
 }
 
-void qs(int ii, int fi, long fV[], int fA[])
-{
-    int i, f;
-    long pi, pa, vtmp, vta, vfi, vfa;
+// ... qs() y main() se mantienen igual ...
 
-    pi = fV[ii];
-    pa = fA[ii];
+// ... Resto del código (qs y main) se mantiene igual ...
 
-    i = ii + 1;
-    f = fi;
+// ... La funció qs() (QuickSort) i main() es mantenen exactament igual ...
+void qs(int ii, int fi, long fV[], int fA[]) {
+    // [Codi original mantingut]
+    int i,f,j;
+    long pi,pa,vtmp,vta,vfi,vfa;
 
-    vtmp = fV[i];
-    vta = fA[i];
+    pi = fV[ii]; pa = fA[ii];
+    i = ii +1; f = fi;
+    vtmp = fV[i]; vta = fA[i];
 
-    while (i <= f)
-    {
-        if (vtmp < pi)
-        {
-            fV[i - 1] = vtmp;
-            fA[i - 1] = vta;
-
-            i++;
-
-            vtmp = fV[i];
-            vta = fA[i];
-        }
-        else
-        {
-            vfi = fV[f];
-            vfa = fA[f];
-
-            fV[f] = vtmp;
-            fA[f] = vta;
-
-            f--;
-
-            vtmp = vfi;
-            vta = vfa;
+    while (i <= f) {
+        if (vtmp < pi) {
+            fV[i-1] = vtmp; fA[i-1] = vta; i ++;
+            vtmp = fV[i]; vta = fA[i];
+        } else {
+            vfi = fV[f]; vfa = fA[f];
+            fV[f] = vtmp; fA[f] = vta; f --;
+            vtmp = vfi; vta = vfa;
         }
     }
+    fV[i-1] = pi; fA[i-1] = pa;
 
-    fV[i - 1] = pi;
-    fA[i - 1] = pa;
-
-    if (ii < f)
-        qs(ii, f, fV, fA);
-
-    if (i < fi)
-        qs(i, fi, fV, fA);
+    if (ii < f) qs(ii,f,fV,fA);
+    if (i < fi) qs(i,fi,fV,fA);
 }
 
-int main()
-{
+int main() {
     int i;
+    // Generació aleatòria
+    for (i=0;i<N;i++) V[i] = (rand()%rand())/N;
+    // primers candidats
+    for (i=0;i<G;i++) R[i] = V[i];
+    // calcular els G mes representatius
+    kmean(N,G,V,R,A);
+    qs(0,G-1,R,A);
 
-    for (i = 0; i < N; i++)
-        V[i] = (rand() % rand()) / N;
+    for (i=0;i<G;i++) 
+        printf("R[%d] : %ld te %d agrupats\n",i,R[i],A[i]);
 
-    // primeros candidatos
-    for (i = 0; i < G; i++)
-        R[i] = V[i];
-
-    // calcular los G más representativos
-    kmean(N, G, V, R, A);
-
-    qs(0, G - 1, R, A);
-
-    for (i = 0; i < G; i++)
-        printf("R[%d] : %ld tiene %d agrupados\n", i, R[i], A[i]);
-
-    return 0;
+    return(0);
 }
