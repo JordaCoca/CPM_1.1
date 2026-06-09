a08adaf77e1df4f409fab9614db6fef68b7d05fb
Jordà Coca <jordacrodriguez02@gmail.com>
2026-06-09 18:39:57 +0200

    a ver q pasa

--- scripts_ejecucion/paralel.sh
+++ scripts_ejecucion/paralel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SRC="./paralelo3.c"
-RESULTS_DIR="./resultados"
-BIN="./pal3"
+SRC="./paralelo4.c"
+RESULTS_DIR="./resultados_4"
+BIN="./paralelo4"
 
 RUNS=10
 
@@ -15,35 +15,41 @@ if [ $? -ne 0 ]; then
 fi
 mkdir -p $RESULTS_DIR
 
-#echo "===== EJECUCIONES EN ORCA ====="
+echo "===== EJECUCIONES EN ORCA ====="
 
-#for t in 2 4 8 16 32 64 128
-#do
-    #echo "Configuracion: $t threads"
-    #{
-        #echo "Programa: paralelo3"
-        #echo "Maquina: orca"
-        #echo "Threads: $t"
-        #echo "Run: $i"
-        #echo "----------------------"
-
-        #srun -p orca -c $t time $BIN
-    #} &> $RESULTS_DIR/orca_${t}
-#done
-
-echo "===== EJECUCIONES EN TEEN ====="
-
-for t in 2 4 8 16 32 
+for t in 1 2 4 8 16 32 64 128
 do
     echo "Configuracion: $t threads"
     {
         echo "Programa: paralelo3"
+        echo "Maquina: orca"
+        echo "Threads: $t"
+        echo "Run: $i"
+        echo "----------------------"
+
+        srun -p orca -c $t time $BIN
+    } &> $RESULTS_DIR/orca_${t}
+done
+
+echo "===== EJECUCIONES EN TEEN ====="
+
+for t in 1 2 4 8 16 32 
+do
+    echo "Configuracion: $t threads"
+    {
+        echo "Programa: paralelo4"
         echo "Maquina: teen"
         echo "Threads: $t"
         echo "Run: $i"
         echo "----------------------"
 
-        srun -p teen -n 1 -c $t time $BIN
+        export OMP_NUM_THREADS=$t
+
+        srun -p teen \
+            --nodes=1 \
+            --ntasks=1 \
+            --cpus-per-task=$t \
+            time -p $BIN
     } &> $RESULTS_DIR/teen_${t}
 done
 
