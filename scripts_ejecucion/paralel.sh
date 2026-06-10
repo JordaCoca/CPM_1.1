e7998f63a7870c4e3185fe7615b49e071e2ffd53
Jordà Coca <jordacrodriguez02@gmail.com>
2026-06-10 20:00:06 +0200

    pff ns

--- scripts_ejecucion/paralel.sh
+++ scripts_ejecucion/paralel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SRC="./paralelo3.c"
-RESULTS_DIR="./resultados_3"
-BIN="./paralelo3"
+SRC="./paralelo4.c"
+RESULTS_DIR="./resultados_4"
+BIN="./paralelo4"
 
 RUNS=10
 
@@ -21,21 +21,12 @@ for t in 1 2 4 8 16 32 64 128
 do
     echo "Configuracion: $t threads"
     {
-        echo "Programa: paralelo3"
+        echo "Programa: paralelo4"
         echo "Maquina: orca"
         echo "Threads: $t"
         echo "----------------------"
 
-        export OMP_NUM_THREADS=$t
-        export OMP_DYNAMIC=FALSE
-        export OMP_PLACES=cores
-        export OMP_PROC_BIND=spread
-
-        srun -p orca \
-            --nodes=1 \
-            --ntasks=1 \
-            --cpus-per-task=$t \
-            /usr/bin/time -p $BIN
+        srun -p orca -c $t time $BIN
 
     } &> $RESULTS_DIR/orca_${t}
 done
@@ -46,19 +37,13 @@ for t in 1 2 4 8 16 32
 do
     echo "Configuracion: $t threads"
     {
-        echo "Programa: paralelo3"
+        echo "Programa: paralelo4"
         echo "Maquina: teen"
         echo "Threads: $t"
         echo "Run: $i"
         echo "----------------------"
 
-        export OMP_NUM_THREADS=$t
-
-        srun -p teen \
-            --nodes=1 \
-            --ntasks=1 \
-            --cpus-per-task=$t \
-            /usr/bin/time -p $BIN
+        srun -p teen -c $t time $BIN
     } &> $RESULTS_DIR/teen_${t}
 done
 
